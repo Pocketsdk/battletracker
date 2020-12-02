9d4edb1b3c3943b620c2f406adc727b4b56b7660
PocketsDK <pocketsdk@outlook.com>
2020-12-02 09:11:46 -0700

    added axios

--- src/components/List/Faction.js
+++ src/components/List/Faction.js
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import "../../tailwind.output.css";
+import { data } from 'autoprefixer';
 
 const Faction = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [faction, setFaction] = useState();
+
+    useEffect(() => {
+        populateFaction();
+    }, []);
+    
+    
+    async function populateFaction() {
+        try {
+          const response = await axios.get('http://localhost/battletracker/server/api/faction/read.php')
+          setFaction(response.data);
+          setLoading(false);
+          setError(false);
+          console.log(faction);
+        } catch (err) {
+          setError(true);
+          setLoading(false);
+        }
+    }
+
+    {/* Loading */}
+    if (loading) {
+        return (
+        <>
+            <p>Fetching your factions...</p>
+        </>
+        );
+    }
+    
+    {/* If Axios request has an error, display error message...*/}
+    if (error) {
+        return (
+        <>
+            <p>Failed fetching factions.</p>
+        </>
+        )
+    } 
+
     return (
         <>
             <form >
                 <h2 className="dougs-header-text">Faction</h2>
-                <select name="factions" id="factions">
+
+                {/* <select name="factions" id="factions">
                     <option value="space-marines">Space Marines</option>
                     <option value="necrons">Necrons</option>
                     <option value="tyranids">Tyranids</option>
-                </select>
+                </select> */}
 
                 <h2 className="dougs-header-text">Subfaction</h2>
                 <select name="sub-factions" id="sub-factions">
