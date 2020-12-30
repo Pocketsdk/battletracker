a3e4edffb69fb51002dc0da77fb6600ec2afe990
Krisztina Pap <krisztina.pap@outlook.com>
2020-12-29 19:28:44 -0700

    Adds database files

---
+++ database/config/credentials.js
@@ -0,0 +1,6 @@
+//Include this Folder in .gitignore! Place required confidential keys / passwords in here.
+const creds = {
+	neo4jusername: "Krisztina",
+	neo4jpw: "Critter1"
+}
+module.exports = { creds };
